91ac6cb6f5a269e6b4e77e5cddcd3aad9019ee58
Sofía Pinilla <contact@sofiapinilla.com>
2020-03-13 09:45:33 +0100

    added some new styles and genres

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-const inicio = document.getElementById('inicio')
-const inicioJ = document.getElementById('inicioJavi')
 const buscar = document.getElementById('buscar').addEventListener('keyup', getPeliculas)
+let genres = []
+const searchMovieURL = 'https://api.themoviedb.org/3/search/movie?api_key=f1cbc5636aa2f2d3b7c9f1c1ca7c91de&language=es-ES&query='
+const genreURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=f1cbc5636aa2f2d3b7c9f1c1ca7c91de&language=en-US'
+const form = document.getElementById('buscarForm')
 
-const URL = 'https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='
-btn.addEventListener('click', getPeliculas)
-inicioJ.addEventListener('click', verInicio)
-
-function verInicio() {
-    inicio.className += 'visible';
+axios.get(genreURL)
+    .then(res => genres = res.data.genres)
+    .catch(err => console.error(err))
+const renderPeliculas = peliculas => {
+    document.querySelector('.peliculas').innerHTML = '';
+    peliculas.forEach(pelicula => {
+        const peliculaGenres = genres.filter(genre => pelicula.genre_ids.includes(genre.id))
+            .map(genre => genre.name)
+            // .reduce((acc, name) => acc + `<h3 class="genre">  ${name} </h3>`, '')
+        let genresH3 = '';
+        peliculaGenres.forEach(genre => genresH3 += `<h3 class="genre"> ${genre} </h3>`);
+        console.log(genresH3);
+        console.log(peliculaGenres)
+        const imagen = pelicula.poster_path ? `<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/${pelicula.poster_path}"/>` : ''
+        document.querySelector('.peliculas').innerHTML += `
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h1>${pelicula.original_title}</h1>
+                <div class="genres">
+                   <h5> ${peliculaGenres}</h5>
+                </div>
+                <button onClick="verDescription(event,'${pelicula.overview}')" class="btn btn-secondary"> Ver descripción</button>
+                <div class="descripcion">
+                </div>
+                <div class="card-img-top">
+                ${imagen}
+                </div>
+                </div>   
+            </div>
+    `
+    })
 }
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const busqueda = event.target.buscar.value
+    axios.get(searchMovieURL + busqueda)
+        .then(res => {
+            const peliculas = res.data.results;
+            renderPeliculas(peliculas)
+        })
+})
 
 function getPeliculas(event) {
     if (event.key === 'Enter') {
         const busqueda = event.target.value
-        document.getElementById('btn')
-        axios.get(URL + busqueda)
+        axios.get(searchMovieURL + busqueda)
             .then(res => {
                 const peliculas = res.data.results;
-                document.querySelector('.peliculas').innerHTML = '';
-                peliculas.forEach(pelicula => {
-                    document.querySelector('.peliculas').innerHTML += `<h1>${pelicula.original_title}</h1><h2>${pelicula.genre_ids}</h2><p>${pelicula.overview}</p><img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/${pelicula.poster_path}"/>
-            `
-                });
+                renderPeliculas(peliculas)
             })
+            .catch(err => console.error(err))
     }
 };
+
+function verDescription(event, descripcion) {
+    console.log(event.target.nextElementSibling)
+    event.target.nextElementSibling.innerHTML = descripcion
+}
